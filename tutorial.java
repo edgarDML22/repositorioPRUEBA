3229359f2d567c1a21fdecb846f7fb102a5e5b5a
Edgar <edgar.ml.2005@outlook.com>
2024-02-16 15:52:35 -0600

    segunda modificación ciclo for

--- tutorial.java
+++ tutorial.java
@@ -8,7 +8,15 @@ public class tutorial {
         System.out.println("HOLA MUNDO OTRA VEZ");
         int edad = 33;
 
-        
+        for (int i = 0; i < 100; i++) {
+            if(edad <= 50){
+                edad++;
+            }
+            else{
+                break;
+            }
+        }
+        System.out.println("La edad después del experimento es: " + edad);
         
 
     }
