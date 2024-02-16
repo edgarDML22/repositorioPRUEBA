d17853d45ea2b7a2e61cf55c63f7c8a3a35fc818
Edgar <edgar.ml.2005@outlook.com>
2024-02-15 21:04:52 -0600

    Tercer commit

--- tutorial.java
+++ tutorial.java
@@ -6,5 +6,6 @@ public class tutorial {
     public static void main(String[] args) {
         System.out.println("HOLA MUNDO");
         System.out.println("HOLA MUNDO OTRA VEZ");
+        int edad = 32;
     }
 }
